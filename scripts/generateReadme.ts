0a64aad22ddec1c45916bf0288d3fc564f91be8f
Jakub Hypki <37691329+jhypki@users.noreply.github.com>
2025-01-25 16:58:43 +0100

    update readme

--- scripts/generateReadme.ts
+++ scripts/generateReadme.ts
@@ -72,7 +72,7 @@ Below is the list of supported languages, taken from \`languages.json\`:
 
   // 2) Append each language's info from JSON
   for (const lang of languages) {
-    content += `- **${lang.name}** (image: \`${lang.dockerImage}\`, file extension: \`.${lang.fileExtension}\`)\n`;
+    content += `- **${lang.name}** (image: \`${lang.dockerImage}\`, language: \`.${lang.language}\`)\n`;
   }
 
   // 3) Optionally add more content, e.g. a license or contact info:
